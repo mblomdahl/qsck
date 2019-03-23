d10a017be62dc5c58dc658d3f161d8c989d03f4c
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-03-23 18:25:44 +0100

    #3 Add `test_serialize.test_it_formats_identifier_and_timestamp_as_unix_epoch` test case

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -1,7 +1,18 @@
 
+from datetime import datetime, timezone
+
 from qsck import serialize
 
 
 def test_serialize_is_a_function():
     assert hasattr(serialize, '__call__')
 
+
+def test_it_formats_identifier_and_timestamp_as_unix_epoch():
+    identifier = 'LOG'
+    timestamp = datetime(2019, 3, 23, 1, 2, 3, tzinfo=timezone.utc)
+    some_key_value_pairs = [("It's Caturday?", 'YES')]
+
+    qs_row = serialize(identifier, timestamp, some_key_value_pairs)
+
+    assert qs_row.startswith("LOG,1553302923,It's Caturday?=")
