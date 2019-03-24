9d75155b6b4af0b7d2d8b042c9788b03fcd5f178
Mats Blomdahl <mats.blomdahl@gmail.com>
2019-03-24 16:37:31 +0100

    #5 Add `test_deserialize.test_it_parses_nested_list_content` test case

--- tests/test_deserialize.py
+++ tests/test_deserialize.py
@@ -1,4 +1,3 @@
-
 from pytest import raises
 
 from qsck import deserialize
@@ -9,7 +8,6 @@ def test_deserialize_is_a_function():
 
 
 def test_it_returns_a_3tuple_with_identifier_timestamp_and_pairs_list():
-
     qs_row = 'LOG,1546902289,_model=LG-M327'
 
     identifier, timestamp, key_value_pairs = deserialize(qs_row)
@@ -22,8 +20,37 @@ def test_it_returns_a_3tuple_with_identifier_timestamp_and_pairs_list():
 
 
 def test_it_rejects_malformatted_qs_row():
-
     qs_row = 'LOG,1546902289'
 
     with raises(AssertionError):
         deserialize(qs_row)
+
+
+def test_it_parses_nested_list_content():
+    qs_row = (
+        'LOG,1546902289,_model=LG-M327,'
+        'event0_vars={subtype=disconnected},'
+        'event10_vars={batteryPct=0.79, isCharging=false, subtype=connected, '
+        'batteryTemp=310, chargeType=Not_Charging},'
+        'event11_data=BATTERY_CHANGED,'
+        'event11_time=1546901849405,'
+        'event11_vars={batteryPct=0.78, batteryTemp=308},'
+        '_rx_host=ip-10-0-1-215'
+    )
+
+    _, __, key_value_pairs = deserialize(qs_row)
+
+    assert key_value_pairs == [
+        ('_model', 'LG-M327'),
+        ("event0_vars", [("subtype", "disconnected")]),
+        ("event10_vars", [("batteryPct", "0.79"),
+                          ("isCharging", "false"),
+                          ("subtype", "connected"),
+                          ("batteryTemp", "310"),
+                          ("chargeType", "Not_Charging")]),
+        ("event11_data", "BATTERY_CHANGED"),
+        ("event11_time", "1546901849405"),
+        ("event11_vars", [("batteryPct", "0.78"),
+                          ("batteryTemp", "308")]),
+        ("_rx_host", "ip-10-0-1-215")
+    ]
